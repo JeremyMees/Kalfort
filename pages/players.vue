fbde13d2d8d36af15b648d45776934b4318c660d
Jeremy <jeremymees123@gmail.com>
2022-06-26 15:15:18 +0200

    players list

---
+++ pages/players.vue
@@ -0,0 +1,13 @@
+<script setup>
+const { pending, data: players } = useLazyFetch('/api/players/get')
+</script>
+
+<template>
+  <div v-if="pending" class="title-normal absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Loading ...</div>
+  <div v-else class="flex flex-col gap-8 max-h-full py-4">
+    <p class="title-normal">{{ players.length }} Brave explorers</p>
+    <div class="grow flex flex-row flex-wrap justify-center gap-4 no-scrollbar overflow-y-auto overscroll-y-auto">
+      <PlayerCard :player="player" v-for="player in players" :key="player.uuid" />
+    </div>
+  </div>
+</template>
